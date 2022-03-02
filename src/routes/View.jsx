8e5d89b185ee985c7c3f3b9e0d474fe2f9c567d6
Luca Joos <1a85ra7z@gmail.com>
2022-03-02 23:22:09 +0100

    Refactor text in View

--- src/routes/View.jsx
+++ src/routes/View.jsx
@@ -87,8 +87,8 @@ const View = () => {
                 </>
             ) : (
                 <>
-                    <Header icon={<Package size={24} />}>View Files</Header>
-                    <p>Someone shared some files with you.<br />Access them individually or download them as an archive.</p>
+                    <Header icon={<Package size={24} />}>Archive</Header>
+                    <p className={'max-w-[400px]'}>These files were found in the archive. You can download them either separately or together.</p>
                     <div className={'flex flex-col gap-2'}>
                         {Object.values(snap.files).map(file => {
                             return <Entry
